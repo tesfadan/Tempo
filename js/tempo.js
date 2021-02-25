78f9cdc126e2bc11fc7567cae9f78a3e39d14b17
Tesfa Demissie <tesfadaniel1@gmail.com>
2021-02-24 20:29:57 -0600

    first commit

---
+++ js/tempo.js
@@ -0,0 +1,143 @@
+function log(x) {
+  console.log(x)
+}
+var firstTap = true;
+var tempoArray = [];
+var tapA;
+var tapB;
+var timeDiff;
+var x = 0;
+var count = 0;
+var taps;
+var bmp;
+var kp = 0;
+
+function findAverage(array) {
+  var average = 0;
+  var y = 0;
+  // log(array);
+  var length = array.length;
+  // log("given array length: " + length);
+
+  while (y < length) {
+    average += array[y];
+    y = y + 1;
+  }
+  average /= length;
+  average = Math.round(average);
+  log("Average is: " + average);
+
+  bmp = average;
+}
+
+var mute = false;
+function audioControl() {
+  if (mute == true) {
+    document.getElementById("audioIcon").src = "icons/speaker.svg";
+    mute = false;
+  }
+  else {
+    document.getElementById("audioIcon").src = "icons/mute.svg";
+    mute = true;
+  }
+}
+
+var keyCount = 0;
+function keyCounter() {
+  if (keyCount > 0) {
+    // document.getElementById("keyboardOption").className += ' fadeOut';
+    // console.log("FADE OUT NOW")
+    document.getElementById("onBoarding").innerHTML = "";
+
+  }
+  keyCount += 1;
+}
+
+function tapped() {
+  console.log("TAP MEE")
+
+  ///SOUND COMES FIRTS FOR FASTER UX
+  var audio = document.getElementById("audio");
+
+  if (mute == false) {
+    audio.play();
+  }
+
+  if (firstTap == true) {
+    document.getElementById("onBoarding").innerHTML = "<h2 class='onBoardingMessage slideup' id='onBoardingKeep'>Keep Going</h2>";
+  }
+  if (x == 0) {
+    // log("Tap0");
+    x = 1;
+    tapA = new Date();
+  }
+
+
+  else if (x == 1) {
+    // log("Tap1");
+    x = 0;
+    firstTap = false;
+
+    tapB = new Date();
+
+  }
+
+
+  if (firstTap == false) {
+
+    if (x == 0) {
+      timeDiff = tapB - tapA;
+
+    }
+    else {
+      timeDiff = tapA - tapB;
+    }
+
+    timeDiff = 60 / (timeDiff / 1000);
+
+
+    if (timeDiff < 500) {
+      tempoArray[count] = timeDiff;
+    }
+
+    if (count < 2) {
+      count += 1;
+    }
+    else {
+      count = 0;
+    }
+
+    findAverage(tempoArray);
+    // document.getElementById("tempoCount").innerHTML = bmp;
+    document.getElementById("main").innerHTML = "<h1 class='count slideupbig' id='tempoCount'>" + bmp + "</h1>"
+
+  }
+  else {
+    console.log("WHHUT")
+  }
+
+}
+
+// function reset() {
+//   firstTap = true;
+//   log("Reset Requested!");
+//   document.getElementById("tempoCount").innerHTML = "";
+//   document.getElementById("tempoBMP").innerHTML = "Click Here";
+
+//   count = 0;
+//   x = 0;
+//   // document.getElementById("resetIcon").setAttribute("style", "display: none;");
+//   // document.getElementById("resetIcon").className = 'audioIcon resetIcon fadeOut';
+//   // document.getElementById("onBoarding").innerHTML = "<h2 class='onBoardingMessage slideup'>Start pressing any key to get beats per minute</h2>";
+// }
+
+
+
+function resetX() {
+  firstTap = true;
+  document.getElementById("onBoarding").innerHTML = "<h2 class='onBoardingMessage slideup' id='onBoardingStart'>Start pressing any key to get beats per minute</h2>";
+  document.getElementById("main").innerHTML = "";
+  console.log("RESET X");
+  count = 0;
+  x = 0;
+}
