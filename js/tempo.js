8754073f1134ad4c12339dd8fbf80e215730a03a
Tesfa Demissie <tesfadaniel1@gmail.com>
2021-02-24 21:07:40 -0600

    mute icon fixed

--- js/tempo.js
+++ js/tempo.js
@@ -1,6 +1,3 @@
-function log(x) {
-  console.log(x)
-}
 var firstTap = true;
 var tempoArray = [];
 var tapA;
@@ -15,51 +12,35 @@ var kp = 0;
 function findAverage(array) {
   var average = 0;
   var y = 0;
-  // log(array);
   var length = array.length;
-  // log("given array length: " + length);
-
   while (y < length) {
     average += array[y];
     y = y + 1;
   }
   average /= length;
   average = Math.round(average);
-  log("Average is: " + average);
 
   bmp = average;
 }
 
 var mute = false;
 function audioControl() {
-  if (mute == true) {
-    document.getElementById("audioIcon").src = "icons/speaker.svg";
-    mute = false;
-  }
-  else {
-    document.getElementById("audioIcon").src = "icons/mute.svg";
-    mute = true;
-  }
+  document.getElementById("audioIcon").src = !mute ? "icons/mute.svg" : "icons/speaker.svg";
+  mute = !mute;
 }
 
 var keyCount = 0;
 function keyCounter() {
-  if (keyCount > 0) {
-    document.getElementById("onBoarding").innerHTML = "";
-  }
-  else {
-    document.getElementById("onBoarding").innerHTML = "<h2 class='onBoardingMessage slideup' id='onBoardingKeep'>Keep Going</h2>";
-  }
+  var element = keyCount > 0 ? "" : "<h2 class='onBoardingMessage slideup' id='onBoardingKeep'>Keep Going</h2>";
+  document.getElementById("onBoarding").innerHTML = element;
   ++keyCount;
 }
 
 function tapped() {
-  console.log("TAP MEE")
-
   ///SOUND COMES FIRTS FOR FASTER UX
   var audio = document.getElementById("audio");
 
-  if (mute == false) {
+  if (!mute) {
     audio.play();
   }
 
@@ -67,7 +48,6 @@ function tapped() {
     document.getElementById("onBoarding").innerHTML = "<h2 class='onBoardingMessage slideup' id='onBoardingKeep'>Keep Going</h2>";
   }
   if (x == 0) {
-    // log("Tap0");
     x = 1;
     tapA = new Date();
   }
@@ -106,21 +86,14 @@ function tapped() {
     else {
       count = 0;
     }
-
     findAverage(tempoArray);
-    // document.getElementById("tempoCount").innerHTML = bmp;
     document.getElementById("main").innerHTML = "<h1 class='count slideupbig' id='tempoCount'>" + bmp + "</h1>"
-
-  }
-  else {
-    console.log("WHHUT")
   }
 }
-function resetX() {
+function reset() {
   firstTap = true;
   document.getElementById("onBoarding").innerHTML = "<h2 class='onBoardingMessage slideup' id='onBoardingStart'>Start pressing any key to get beats per minute</h2>";
   document.getElementById("main").innerHTML = "";
-  console.log("RESET X");
   count = 0;
   keyCount = 0;
   x = 0;
